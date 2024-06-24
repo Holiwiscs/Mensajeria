28f48e87fc042fc959d232827eeeff9d9cfaec95
Holiwiscs <feli.pinos@duocuc.cl>
2024-06-24 18:54:37 -0400

    chatBox listo con el css

--- src/app/pages/menu/chat/chat.page.ts
+++ src/app/pages/menu/chat/chat.page.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPage implements OnInit {
 
+  name: string = 'Sender';
+  message: string;
+  isLoading = false;
+  currentUserId = 1;
+  chats = [
+    {id: 1, sender: 1, message: 'h1'},
+    {id: 2, sender: 2, message: 'h1 there!'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sendMessage(){
+
   }
 
 }
